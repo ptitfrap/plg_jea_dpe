5dd2a4463abb208f0531929119687d5e7f71a50a
Sylvain Philip <contact@sphilip.com>
2011-01-22 16:23:30 +0000

    Add GPL licence to plugin jea_dpe

--- dpe.php
+++ dpe.php
@@ -1,4 +1,16 @@
 <?php
+/**
+ * This file is part of Joomla Estate Agency - Joomla! extension for real estate agency
+ * 
+ * @package		Jea
+ * @copyright	Copyright (C) 2011 PHILIP Sylvain. All rights reserved.
+ * @license		GNU/GPL, see LICENSE.txt
+ * Joomla Estate Agency is free software. This version may have been modified pursuant to the
+ * GNU General Public License, and as distributed it includes or is derivative
+ * of works licensed under the GNU General Public License or other free or open
+ * source software licenses.
+ * 
+ */
 
 defined( '_JEXEC' ) or die( 'Restricted access' );
 
