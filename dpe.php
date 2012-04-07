949c246b4c3bd72c64c255e4bdaa9b10bf389b79
Sylvain Philip <contact@sphilip.com>
2012-04-07 13:10:44 +0000

    Plugins dpe and example migration

--- dpe.php
+++ dpe.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * This file is part of Joomla Estate Agency - Joomla! extension for real estate agency
- * 
+ *
  * @package		Jea
  * @copyright	Copyright (C) 2011 PHILIP Sylvain. All rights reserved.
  * @license		GNU/GPL, see LICENSE.txt
@@ -9,68 +9,54 @@
  * GNU General Public License, and as distributed it includes or is derivative
  * of works licensed under the GNU General Public License or other free or open
  * source software licenses.
- * 
+ *
  */
 
 defined( '_JEXEC' ) or die( 'Restricted access' );
 
 
-
 jimport('joomla.event.plugin');
-JPlugin::loadLanguage('plg_jea_dpe', JPATH_ADMINISTRATOR);
+// JPlugin::loadLanguage('plg_jea_dpe', JPATH_ADMINISTRATOR);
 
 class plgJeaDpe extends JPlugin
 {
-
     /**
-     * onInitTableProperty Method
+     * Constructor
      *
-     * @param TableProperties $table
+     * @param       object  $subject The object to observe
+     * @param       array   $config  An array that holds the plugin configuration
      */
-    function onInitTableProperty(&$table)
+    public function __construct(& $subject, $config)
     {
-        $db =& JFactory::getDBO();
-        
-        $db->setQuery('SHOW COLUMNS FROM #__jea_properties');
-        $cols = $db->loadObjectList('Field');
-        if(!isset($cols['dpe_energie']) && !isset($cols['dpe_ges'])){
-            $query = 'ALTER TABLE `#__jea_properties` '
-                   . "ADD `dpe_energie` ENUM('0','A','B','C','D','E','F','G') NOT NULL DEFAULT '0',"
-                   . "ADD `dpe_ges`     ENUM('0','A','B','C','D','E','F','G') NOT NULL DEFAULT '0'";
-
-            $db->setQuery($query);
-            $db->query();
-        }
-        $table->dpe_energie = null;
-        $table->dpe_ges = null;
+        parent::__construct($subject, $config);
+        $this->loadLanguage();
     }
-    
+
     /**
      * onBeforeSaveProperty method
      *
+     * @param string $namespace
      * @param TableProperties $row
      * @param boolean $is_new
      * @return boolean
      */
-    function onBeforeSaveProperty(&$row, $is_new)
+    function onBeforeSaveProperty($namespace, &$row, $is_new)
     {
-        $row->dpe_energie = JRequest::getVar( 'dpe_energie', '', 'POST' );
-        $row->dpe_ges     = JRequest::getVar( 'dpe_ges', '', 'POST' );
+        $row->dpe_energie = JRequest::getVar( 'dpe_energie', '');
+        $row->dpe_ges     = JRequest::getVar( 'dpe_ges', '');
         return true;
     }
-    
-    
 
     /**
      * onBeforeEndPane method (Called in the admin property form)
      *
-     * @param JPane $pane
      * @param TableProperties $row
+     * @return void
      */
-    function onBeforeEndPane(&$pane, &$row)
+    function onBeforeEndPanels(&$row)
     {
         $list = array();
-        $list[] = JHTML::_('select.option', '0', '-- ' . JText::_('Select class') . ' --' );
+        $list[] = JHTML::_('select.option', '0', '-- ' . JText::_('SELECT_CLASS') . ' --' );
         $list[] = JHTML::_('select.option', 'A', 'A' );
         $list[] = JHTML::_('select.option', 'B', 'B' );
         $list[] = JHTML::_('select.option', 'C', 'C' );
@@ -85,24 +71,22 @@ class plgJeaDpe extends JPlugin
         $html ='
          <table>
           <tr>
-    		<td class="label" ><label for="energie_dpe">' . JText::_('Consommation energetique') .' : </label></td>
+    		<td class="label" ><label for="energie_dpe">' . JText::_('CONSOMMATION_ENERGETIQUE') .' : </label></td>
     		<td>'.$energie_list.'</td>
     	  </tr>
     	  
     	  <tr>
-    		<td class="label"><label for="energie_dpe">' . JText::_('Emissions GES') .' : </label></td>
+    		<td class="label"><label for="energie_dpe">' . JText::_('EMISSIONS_GES') .' : </label></td>
     		<td>'.$ges_list.'</td>
     	  </tr>
 			
     	</table>';
 
-        echo $pane->startPanel( JText::_('DPE') , "dpe-pane" );
+        echo JHtml::_('sliders.panel', JText::_('DPE'), 'dpe-pane');
         echo $html;
-        echo $pane->endPanel();
-
     }
 
-    
+
     /**
      * onAfterShowDescription method (called in the default_item.php tpl)
      *
@@ -111,21 +95,21 @@ class plgJeaDpe extends JPlugin
     function onAfterShowDescription(&$row)
     {
         if (empty($row->dpe_energie) && empty($row->dpe_ges)) {
-            return; 
+            return;
         }
-        
+
         echo '<h3 class="jea_dpe">'. JText::_('DPE') .'</h3>'. PHP_EOL;
-        
+
         echo '<div class="jea_dpe">'. PHP_EOL;
-             
+         
         if(!empty($row->dpe_energie)) {
             echo JHTML::_('image.site', 'energie_'.$row->dpe_energie.'.gif', '/plugins/jea/dpe/images/');
         }
-             
+         
         if(!empty($row->dpe_ges)) {
             echo JHTML::_('image.site', 'ges_'.$row->dpe_ges.'.gif', '/plugins/jea/dpe/images/');
         }
-        
+
         echo '</div>'. PHP_EOL;
     }
 
---
+++ script.php
@@ -0,0 +1,85 @@
+<?php
+/**
+ * This file is part of Joomla Estate Agency - Joomla! extension for real estate agency
+ * 
+ * @version     $Id: script.php 257 2012-02-05 23:04:04Z ilhooq $
+ * @package		Jea
+ * @copyright	Copyright (C) 2008 PHILIP Sylvain. All rights reserved.
+ * @license		GNU/GPL, see LICENSE.txt
+ * Joomla Estate Agency is free software. This version may have been modified pursuant to the
+ * GNU General Public License, and as distributed it includes or is derivative
+ * of works licensed under the GNU General Public License or other free or open
+ * source software licenses.
+ * 
+ */
+
+// No direct access to this file
+defined('_JEXEC') or die('Restricted access');
+ 
+/**
+ * Install Script file of JEA component
+ */
+class plg_jeaDpeInstallerScript
+{
+	/**
+	 * method to install the extension
+	 *
+	 * @return void
+	 */
+	function install($parent) 
+	{
+    	$db = JFactory::getDbo();
+        $db->setQuery('SHOW COLUMNS FROM #__jea_properties');
+        $cols = $db->loadObjectList('Field');
+        if(!isset($cols['dpe_energie']) && !isset($cols['dpe_ges'])){
+            $query = 'ALTER TABLE `#__jea_properties` '
+                   . "ADD `dpe_energie` ENUM('0','A','B','C','D','E','F','G') NOT NULL DEFAULT '0',"
+                   . "ADD `dpe_ges`     ENUM('0','A','B','C','D','E','F','G') NOT NULL DEFAULT '0'";
+
+            $db->setQuery($query);
+            $db->query();
+        }
+	}
+ 
+	/**
+	 * method to uninstall the extension
+	 *
+	 * @return void
+	 */
+	function uninstall($parent) 
+	{
+		
+	}
+ 
+	/**
+	 * method to update the extension
+	 *
+	 * @return void
+	 */
+	function update($parent) 
+	{
+
+	}
+ 
+	/**
+	 * method to run before an install/update/uninstall method
+	 *
+	 * @return void
+	 */
+	function preflight($type, $parent) 
+	{
+
+	}
+ 
+	/**
+	 * method to run after an install/update/uninstall method
+	 *
+	 * @return void
+	 */
+	function postflight($type, $parent) 
+	{
+
+	}
+}
+
+
